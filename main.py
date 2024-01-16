05410c9be783e6d7e76194737284a690dcf6da5b
OtgonzayaB <otgonzaya6052@gmail.com>
2024-01-16 13:15:04 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# to delete everthing in a folder use "rm -r foldername"
+# Initialize git repository
+# git init
+#Check git repository status "git status"
+#Create text file from the shell using touch "touch READ.md"
+# Did git catch the README.md file? "git status"
+# Stage the first file "git add README.md"
+#Commit the file to the .git repo "git commit -m "first commit""
+# Please tell me who you are "git config --global user.email "
+# Commit it "git commit -m "added README.md"
+# Check commit history "git log"
+# Check what changed since the last commit "git diff README.md"
+#Re-create the original folder structure using -p option for rmkdir
+# Install csvcut using pip install csvcut
