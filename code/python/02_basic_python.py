03a069801b5310a027c443049821a296862844ac
Otgonzaya Battulga <Battulga_Otgo@student.ceu.edu>
2024-02-13 15:00:49 +0100

    Added python files

---
+++ code/python/02_basic_python.py
@@ -0,0 +1,43 @@
+# Have a look at logical statements
+print(2 == 2) #True
+print(2 == 3) #False
+
+# Check if True and 1 are in fact equal
+print(True == 1) #True
+
+# True evaluates to 1 and False evaluates to 0.
+print(True + True + False)
+
+# Check equivalence of two variables
+a = "Hi"
+b = "Hello"
+print(a == b)
+
+# Multiple logical statements
+print(2 == 2 and 3 == 3) #True
+print(2 == 2 and 3 == 4) #False
+
+# Testing for inequality 
+print(2 != 3) #True
+
+# Testing for less than/more than
+print(2 < 3 ) #True
+print(2 > 3) #False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
